614f0d460b5ab516d021c466ae1c81b778f4e65a
Jiri Palek <jiri.palek@gmail.com>
2019-09-09 13:26:28 +0200

    Add test for series shape

--- DT - Date and Time/test_TimeSeries.py
+++ DT - Date and Time/test_TimeSeries.py
@@ -1,5 +1,7 @@
 import datetime
 
+import pytest
+
 import TimeSeries as TS
 import TimeSeriesGenerator as TSG
 
@@ -16,6 +18,28 @@ def __get_ts_df():
     return df
 
 
+def __get_ts_series():
+    tsg = TSG.TimeSeriesGenerator()
+    list_ts = tsg.generate_month_data()
+
+    ts = TS.TimeSeries()
+    ts.set_ts_list(list_ts, ["TIME", "VALUE"])
+
+    series = ts.get_ts_series()
+
+    return series
+
+
+def test_ts_series_shape():
+    series = __get_ts_series()
+    assert series.shape == (30,)
+
+
+@pytest.mark.skip("WIP")
+def test_ts_series_name():
+    pass
+
+
 def test_ts_df_shape():
     df = __get_ts_df()
     assert df.shape == (30, 2)
@@ -24,3 +48,8 @@ def test_ts_df_shape():
 def test_ts_df_date_type():
     df = __get_ts_df()
     assert isinstance(df["TIME"][0], datetime.datetime)
+
+
+@pytest.mark.skip("WIP")
+def test_ts_df_names():
+    pass
