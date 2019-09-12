560758cd6e344aa9a374ba5f879d21c4de72d20a
Jiri Palek <jiri.palek@gmail.com>
2019-09-12 10:52:47 +0200

    Add fit test for day data in DWMY transformator

--- DT - Date and Time/DWMYTransformator.py
+++ DT - Date and Time/DWMYTransformator.py
@@ -1,6 +1,31 @@
 from BaseTransformator import BaseTransformator
 
+
 class DWMYTransformator(BaseTransformator):
 
     def __init__(self):
         pass
+
+    def __transform(self, X, type):
+        """
+
+        :param X:
+        :param type:
+        :return:
+        """
+        if type == "d":
+            return ((X.year + X.month / 100 + X.day / 10000) * 10000).astype(int)
+
+    def fit(self, X, type):
+        """
+        :param X:
+        :param type:
+        :return:
+        """
+        return self.__transform(X, type)
+
+    def fitpredict(self, X, type):
+        return self.__transform(X, type)
+
+    def predict(self, X, type):
+        return self.__transform(X, type)
--- DT - Date and Time/test_DWMTTransformator.py
+++ DT - Date and Time/test_DWMTTransformator.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+import DWMYTransformator as T
 import TimeSeries as TS
 import TimeSeriesGenerator as TSG
 
@@ -5,6 +8,7 @@ import TimeSeriesGenerator as TSG
 def __get_ts_array(type):
     tsg = TSG.TimeSeriesGenerator()
     ts = TS.TimeSeries()
+    t = T.DWMYTransformator()
 
     if type == "d":
         ts.set_ts_list(tsg.generate_day_data(), ["TIME", "VALUE"])
@@ -14,9 +18,16 @@ def __get_ts_array(type):
         ts.set_ts_list(tsg.generate_month_data(), ["TIME", "VALUE"])
 
     ts_d = ts.get_ts_df()
+    transformed_data = t.fit(ts_d["TIME"].array, type)
 
-    return ts_d["TIME"].array
+    return transformed_data
 
 
 def test_day_conversion():
-    assert True
+    days = __get_ts_array("d")
+    results = np.array([20190801, 20190805, 20190809, 20190811, 20190813, 20190822, 20190824, 20190825,
+                        20190826, 20190827, 20190827, 20190828, 20190829, 20190829, 20190901, 20190901, 20190903,
+                        20190909, 20190909, 20190911, 20190912, 20190913, 20190917, 20190917, 20190922, 20190922,
+                        20190923, 20190924, 20190929, 20190930])
+
+    assert np.array_equal(days, results)
