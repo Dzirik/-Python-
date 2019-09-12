c89032faa66868c314750664c1ff73c83df5be4c
Jiri Palek <jiri.palek@gmail.com>
2019-09-12 14:46:39 +0200

    Add and test year generator to DateTimeGenerator

--- DT - Date and Time/DateTimeGenerator.py
+++ DT - Date and Time/DateTimeGenerator.py
@@ -1,7 +1,13 @@
+import random
+from datetime import datetime
+
 class DateTimeGenerator():
 
     def __init__(self):
         pass
+
+    def generate_year(self, year_from=1900, year_to=datetime.now().year):
+        return random.randint(year_from, year_to)
 
 def __is_leap(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
--- DT - Date and Time/test_DateTimeGenerator.py
+++ DT - Date and Time/test_DateTimeGenerator.py
@@ -1,15 +1,37 @@
-import DateTimeGenerator as DTG
+import random
+
+import pytest
+
+from DateTimeGenerator import DateTimeGenerator
 from DateTimeGenerator import __is_leap
+
+
+@pytest.fixture
+def dtg():
+    return DateTimeGenerator()
+
+
+def test_year_generator(dtg):
+    random.seed(321)
+    year_from = 2000
+    year_to = 2025
+    years_gen = set([dtg.generate_year(year_from, year_to) for i in range(200)])
+    years_test = set(range(year_from, year_to + 1))
+
+    assert years_gen == years_test
+
 
 def test_standard_leap_year():
     assert __is_leap(1996)
 
+
 def test_non_standard_leap_year():
     assert __is_leap(2000)
+
 
 def test_non_leap_year():
     assert not __is_leap(1997)
 
+
 def test_non_standart_non_leap_year():
     assert not __is_leap(1900)
-
