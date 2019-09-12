38ed38916ab7ca196fb840fac974812b28780249
Jiri Palek <jiri.palek@gmail.com>
2019-09-12 14:53:52 +0200

    Add code and test for generate month

--- DT - Date and Time/DateTimeGenerator.py
+++ DT - Date and Time/DateTimeGenerator.py
@@ -6,8 +6,14 @@ class DateTimeGenerator():
     def __init__(self):
         pass
 
-    def generate_year(self, year_from=1900, year_to=datetime.now().year):
+    @staticmethod
+    def generate_year(year_from=1900, year_to=datetime.now().year):
         return random.randint(year_from, year_to)
+
+    @staticmethod
+    def generate_month(month_from=1, month_to=12):
+        return random.randint(month_from, month_to)
+
 
 def __is_leap(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
--- DT - Date and Time/test_DateTimeGenerator.py
+++ DT - Date and Time/test_DateTimeGenerator.py
@@ -20,6 +20,15 @@ def test_year_generator(dtg):
 
     assert years_gen == years_test
 
+def test_month_generator(dtg):
+    random.seed(4325)
+    month_from = 1
+    month_to = 12
+    months_gen = set([dtg.generate_month(month_from, month_to) for i in range(100)])
+    months_test = set(range(month_from, month_to+1))
+
+    assert months_gen == months_test
+
 
 def test_standard_leap_year():
     assert __is_leap(1996)
