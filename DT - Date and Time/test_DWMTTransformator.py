47081b2e450c9e472b76e81eb5319d64db221e48
Jiri Palek <jiri.palek@gmail.com>
2019-09-12 09:00:26 +0200

    Add data to test

--- DT - Date and Time/test_DWMTTransformator.py
+++ DT - Date and Time/test_DWMTTransformator.py
@@ -1,5 +1,22 @@
+import TimeSeries as TS
 import TimeSeriesGenerator as TSG
-import DWMYTransformator as t
+
+
+def __get_ts_array(type):
+    tsg = TSG.TimeSeriesGenerator()
+    ts = TS.TimeSeries()
+
+    if type == "d":
+        ts.set_ts_list(tsg.generate_day_data(), ["TIME", "VALUE"])
+    elif type == "w":
+        ts.set_ts_list(tsg.generate_week_data(), ["TIME", "VALUE"])
+    elif type == "m" or type == "y":
+        ts.set_ts_list(tsg.generate_month_data(), ["TIME", "VALUE"])
+
+    ts_d = ts.get_ts_df()
+
+    return ts_d["TIME"].array
+
 
 def test_day_conversion():
     assert True
