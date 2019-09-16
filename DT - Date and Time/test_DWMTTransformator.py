64bc26e6fcf343496a2449989816f6a69b3a8d0f
Jiri Palek <jiri.palek@gmail.com>
2019-09-16 09:23:13 +0200

    Add month transformation and testing

--- DT - Date and Time/test_DWMTTransformator.py
+++ DT - Date and Time/test_DWMTTransformator.py
@@ -9,6 +9,10 @@ ground_truth_day = np.array([20190801, 20190805, 20190809, 20190811, 20190813, 2
                              20190826, 20190827, 20190827, 20190828, 20190829, 20190829, 20190901, 20190901, 20190903,
                              20190909, 20190909, 20190911, 20190912, 20190913, 20190917, 20190917, 20190922, 20190922,
                              20190923, 20190924, 20190929, 20190930])
+ground_truth_month = np.array([201801, 201803, 201803, 201805, 201806, 201809, 201809, 201810, 201810,
+                               201811, 201811, 201812, 201812, 201901, 201901, 201901, 201901, 201904, 201904,
+                               201905, 201906, 201906, 201907, 201908, 201909, 201909, 201909, 201909,
+                               201911, 201911])
 
 
 def __get_ts_array(time_type):
@@ -44,7 +48,10 @@ def __transform_ts_array(ts_array, transformation_type, time_type):
                          [
                              ("f", "d", ground_truth_day),
                              ("fp", "d", ground_truth_day),
-                             ("p", "d", ground_truth_day)
+                             ("p", "d", ground_truth_day),
+                             ("f", "m", ground_truth_month),
+                             ("fp", "m", ground_truth_month),
+                             ("p", "m", ground_truth_month)
                          ])
 def test_fit_day_conversion(transformation_type, time_type, ground_truth):
     time_array = __get_ts_array(time_type)
