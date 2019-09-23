a50e5674d0018c1819e210127c0fb5c794ff9c1c
Jiri Palek <jiri.palek@gmail.com>
2019-09-23 09:41:35 +0200

    Add TSToIndex transformator structure

---
+++ DT - Date and Time/TSToIndexTransformator.py
@@ -0,0 +1,19 @@
+from BaseTransformator import BaseTransformator
+
+class TSToIndexTransformator(BaseTransformator):
+
+    def __init__(self):
+        BaseTransformator.__init__(self, name="TSToIndex", data_type="???")
+
+    def __transform(self):
+        pass
+
+    def fit(self):
+        return self.__transform()
+
+    def fitpredict(self):
+        return self.__transform()
+
+    def predict(self):
+        return self.__transform()
+
