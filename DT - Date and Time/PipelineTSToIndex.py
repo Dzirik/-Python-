0ff9b4180861db9efa153b5995a47a159fd5cf05
Jiri Palek <jiri.palek@gmail.com>
2019-09-24 09:04:06 +0200

    Add reading from triplet in pipeline

--- DT - Date and Time/PipelineTSToIndex.py
+++ DT - Date and Time/PipelineTSToIndex.py
@@ -2,6 +2,7 @@ import DWMYDFTransformator as DWMYDFT
 import DWMYGroupTransformator as DWMYGgroup
 import TSToIndexTransformator as TSToIndex
 import TimeSeries as TS
+import TripletSaverAndLoader as TripletSaverAndLoader
 
 
 class PipelineTSToIndex():
@@ -11,13 +12,17 @@ class PipelineTSToIndex():
         self.dwmy_df_t = DWMYDFT.DWMYDFTransformator()
         self.dwmy_group_t = DWMYGgroup.DWMYGroupTransformator()
         self.ts_to_index_t = TSToIndex.TSToIndexTransformator()
+        self.tri_save_load = TripletSaverAndLoader.TripletSaverAndLoader(None)
 
-    def execute(self, df, time_type, atr_names_df, atr_names_ts, group_fun, fun_type="entropy"):
+    def execute(self, file_name, time_type, atr_names_df, atr_names_ts, group_fun, fun_type="entropy"):
+        # TODO - fix naming here and in triplet saver and loader - that "TS_"
+        df = self.tri_save_load.load_triplet(file_name, atr_names_df[0], atr_names_df[1][3:])
         df_idx = df[atr_names_df[1]].apply(
             self._create_index_for_ts_list,
             args=(time_type, atr_names_ts, group_fun, fun_type)
         )
         df_idx = df_idx.to_frame()
+        df_idx.rename(columns={atr_names_df[1]: (atr_names_df[1][3:] + "_" + fun_type.upper())}, inplace=True)
         df_idx[atr_names_df[0]] = df[atr_names_df[0]]
 
         return df_idx
