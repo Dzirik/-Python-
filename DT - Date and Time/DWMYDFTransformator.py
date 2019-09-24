30b88dc6700b8932011a57a593dbd16f27767766
Jiri Palek <jiri.palek@gmail.com>
2019-09-24 16:26:10 +0200

    refactoring, but bad

--- DT - Date and Time/DWMYDFTransformator.py
+++ DT - Date and Time/DWMYDFTransformator.py
@@ -1,3 +1,4 @@
+# TODO - refactor transform
 from DWMYTransformator import DWMYTransformator
 
 
@@ -8,14 +9,16 @@ class DWMYDFTransformator(DWMYTransformator):
         self.name = "DWMY"
         self.data_type = "df"
 
-    def __transform(self, X, time_type, atr_names):
+    def transform(self, X, time_type, atr_names):
         """
 
         :param X:
         :param time_type:
         :return:
         """
-        X[atr_names[0] + "_" + time_type] = super(DWMYDFTransformator, self).fit(X[atr_names[0]].array, time_type)
+        # X[atr_names[0] + "_" + time_type] = super(DWMYDFTransformator, self).fit(X[atr_names[0]].array, time_type)
+        # X[atr_names[0] + "_" + time_type] = DWMYTransformator.fit(self, X[atr_names[0]].array, time_type)
+        X[atr_names[0] + "_" + time_type] = super().fit(X[atr_names[0]].array, time_type)
         X.drop([atr_names[0]], axis=1, inplace=True)
         X.reset_index(drop=True, inplace=True)
 
@@ -27,10 +30,10 @@ class DWMYDFTransformator(DWMYTransformator):
         :param time_type:
         :return:
         """
-        return self.__transform(X, time_type, atr_names)
+        return self.transform(X, time_type, atr_names)
 
     def fit_predict(self, X, time_type, atr_names):
-        return self.__transform(X, time_type, atr_names)
+        return self.transform(X, time_type, atr_names)
 
     def predict(self, X, time_type, atr_names):
-        return self.__transform(X, time_type, atr_names)
+        return self.transform(X, time_type, atr_names)
